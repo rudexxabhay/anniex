c628f82eb48b576e99a41e6b3e423da8af1f04ef
ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ꜱɪɴɢʜ <ks8817112@gmail.com>
2024-07-19 17:40:16 +0530

    ping.py

--- ANNIEMUSIC/plugins/tools/ping.py
+++ ANNIEMUSIC/plugins/tools/ping.py
@@ -16,8 +16,8 @@ from config import BANNED_USERS
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    response = await message.reply_video(
-        video="https://telegra.ph/file/0aace4c6180763afedd8c.jpg",
+    response = await message.reply_photo(
+        photo="https://telegra.ph/file/5df6efaed828b72d613e7.jpg",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await JARVIS.ping()
